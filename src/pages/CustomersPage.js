3cd2681ffb03c77b43f220c92f5756c50742f3ca
Erik Fubel <erik.fubel@gmail.com>
2023-05-24 12:50:07 +0200

    Fetch customer data from API

--- src/pages/CustomersPage.js
+++ src/pages/CustomersPage.js
@@ -1,11 +1,29 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Body from "../components/Body";
 import CustomerList from "../components/CustomerList"
 import Spinner from '../components/Spinner';
 
 
+const BASE_API_URL = process.env.REACT_APP_BASE_API_URL
+
 export default function CustomersPage() {
     const [customers, setCustomers] = useState()
+
+    // Fetch customer data
+    useEffect(() => {
+        (async () => {
+          const response = await fetch(BASE_API_URL + '/api/customers');
+          console.log(response)
+          if (response.ok) {
+            const results = await response.json();
+            setCustomers(results);
+          }
+          else {
+            setCustomers(null);
+          }
+        })();
+      }, []);
+
     return (
         <Body sidebar>
             {customers === undefined ? 
