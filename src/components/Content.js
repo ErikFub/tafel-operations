98cfd1823354de1f45d3db0d57b289746fd038d5
Erik Fubel <erik.fubel@gmail.com>
2023-05-24 13:44:49 +0200

    Invert white vs. gray color scheme

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,10 +1,8 @@
 export default function Content({ children }) {
     return (
         <main>
-            <div class="p-4 sm:ml-64">
-                <div class="p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700">
-                    {children}
-                </div>
+            <div class="p-10 sm:ml-64">
+                {children}
             </div>
         </main>
     )
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -9,7 +9,7 @@ export default function Sidebar() {
     ]
     return (
         <aside id="default-sidebar" class="fixed top-0 left-0 z-40 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0" aria-label="Sidebar">
-            <div class="h-full px-3 py-4 overflow-y-auto bg-gray-50 dark:bg-gray-800">
+            <div class="h-full px-3 py-4 overflow-y-auto bg-white shadow dark:bg-gray-800">
                 <a href="/" class="flex items-center pl-2.5 mb-5">
                     <img src="https://www.tafel.de/fileadmin/media/Ueber_uns/Aktuelle_Meldungen/2019/2019-05-13_Tafel-App.png" class="h-6 mr-3 sm:h-7" alt="Tafel Logo" />
                     <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Tafel Operations</span>
