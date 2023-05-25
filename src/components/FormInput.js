52d1c5ee2aa5192b91be5316d4247e04576cc113
Erik Fubel <erik.fubel@gmail.com>
2023-05-25 18:39:50 +0200

    Add placdholder for country input

--- src/components/FormInput.js
+++ src/components/FormInput.js
@@ -1,9 +1,9 @@
-export default function FormInput({ label, id, placeholder, required, type}) {
+export default function FormInput({ label, id, placeholder, required, type, autofocus}) {
     const idAdj = id ? id : label.replace(' ', '-').toLowerCase()
     return (
         <div>
             <label for={idAdj} class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">{label}</label>
-            <input type={type} name={idAdj} id={idAdj} class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" required={required} placeholder={placeholder} />
+            <input type={type} name={idAdj} id={idAdj} class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" required={required} placeholder={placeholder} autofocus={autofocus} />
         </div>
     )
 }
--- src/components/NewCustomerModal.js
+++ src/components/NewCustomerModal.js
@@ -61,12 +61,12 @@ export default function NewCustomerModal({ setShowModal, handleAddCustomer }) {
         <Modal setShowModal={setShowModal}>
             <h3 class="mb-4 text-xl font-medium text-gray-900 dark:text-white">Create new customer</h3>
             <form class="space-y-6" onSubmit={handleCreate}>
-                <FormInput label={"First Name"} type={"text"} required />
+                <FormInput label={"First Name"} type={"text"} required autofocus={true} />
                 <FormInput label={"Last Name"} type={"text"} required />
                 <FormInput label={"Street"} type={"text"} />
                 <FormInput label={"ZIP"} type={"text"} />
                 <FormInput label={"City"} type={"text"} />
-                <FormInput label={"Country"} type={"text"} />
+                <FormInput label={"Country"} type={"text"} placeholder={'DE'} />
                 <button type="submit" class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Create Customer</button>
             </form>
         </Modal>
