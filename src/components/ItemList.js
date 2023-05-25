d3945cc94bdd54adb1fb3e362d032f2b2d96a5bd
Erik Fubel <erik.fubel@gmail.com>
2023-05-25 19:13:54 +0200

    Remove Y-axis padding from item list

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -1,6 +1,6 @@
 export default function ItemList({ children }) {
     return (
-        <ul className="divide-y divide-gray-200 rounded-lg bg-white shadow p-5">
+        <ul className="divide-y divide-gray-200 rounded-lg bg-white shadow px-5 py-1">
             {children}
         </ul>
     )
