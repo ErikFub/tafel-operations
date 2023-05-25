2fc6c4109e9584dc214ea63ce090e24062680ed5
Erik Fubel <erik.fubel@gmail.com>
2023-05-25 21:05:11 +0200

    Add search bar functionality

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -1,7 +1,19 @@
+import { Children } from "react"
+
 export default function ItemList({ children }) {
     return (
         <ul className="divide-y divide-gray-200 rounded-lg bg-white shadow px-5 py-1">
-            {children}
+            {Children.count(children) === 0 ?
+            <li class="p-3 columns-2 flex">
+                <div className="w-full content-center grid">
+                    <p className="text-base font-bold">
+                        No results
+                    </p>
+                </div>
+            </li>
+            :
+            children}
         </ul>
+        
     )
 }
--- src/components/ListHeader.js
+++ src/components/ListHeader.js
@@ -1,21 +1,30 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { icon } from '@fortawesome/fontawesome-svg-core/import.macro'
+import { SearchBar } from './SearchBar'
 
-export default function ListHeader({ title, newButton, setShowNewModal }) {
+export default function ListHeader({ title, newButton, setShowNewModal, searchBar, searchText, onSearchTextChange }) {
     return (
-        <div className="columns-2 flex mb-3 mx-2">
-            <div className="w-full">
-                <h1 className="text-base font-bold text-gray-700 tracking-wide">
-                    {title.toUpperCase()}
-                </h1>
+        <>
+            <h1 className="text-base font-bold text-gray-700 tracking-wide">
+                {title.toUpperCase()}
+            </h1>
+            <div className="columns-2 flex mb-3 mt-2">
+                {(searchBar) ?
+                <div className='w-full mr-5'>
+                    <SearchBar searchText={searchText} onSearchTextChange={onSearchTextChange}></SearchBar>
+                </div>
+                :
+                null
+                }
+
+                {(newButton) ? 
+                <button onClick={() => setShowNewModal(true)} type="button" class="text-white bg-[#FF9119] hover:bg-[#FF9119]/80 focus:ring-4 focus:outline-none focus:ring-[#FF9119]/50 font-medium rounded-lg text-sm px-3.5 py-2 text-center inline-flex items-center dark:hover:bg-[#FF9119]/80 dark:focus:ring-[#FF9119]/40">
+                        <FontAwesomeIcon icon={icon({name: 'plus'})} className='mr-1' /><p className='font-bold'>New</p>
+                </button>
+                :
+                null
+                }
             </div>
-            {(newButton) ? 
-            <button onClick={() => setShowNewModal(true)} type="button" class="text-white bg-[#FF9119] hover:bg-[#FF9119]/80 focus:ring-4 focus:outline-none focus:ring-[#FF9119]/50 font-medium rounded-lg text-sm px-3.5 py-2 text-center inline-flex items-center dark:hover:bg-[#FF9119]/80 dark:focus:ring-[#FF9119]/40 mr-2">
-                    <FontAwesomeIcon icon={icon({name: 'plus'})} className='mr-1' /><p className='font-bold'>New</p>
-            </button>
-            :
-            null
-            }
-        </div>
+        </>
     )
 }
