f679eabb4669884271bf6d4a1ae667f79441107f
Erik Fubel <erik.fubel@gmail.com>
2023-05-26 14:25:38 +0200

    Fix styling of three-dot menu

--- src/components/ThreeDotMenu.js
+++ src/components/ThreeDotMenu.js
@@ -1,19 +1,25 @@
 import { useState } from "react";
 import ThreeDotMenuButton from "./ThreeDotMenuButton";
-import ThreeDotMenuDropdown from "./ThreeDotMenuDropdown";
+import MenuDropdown from "./MenuDropdown";
+import '../index.css';
 
 export default function ThreeDotMenu({ children }) {
     const [showMenu, setShowMenu] = useState(false)
 
     return (
-        <>
+        <div>
             <ThreeDotMenuButton handleClick={() => setShowMenu(!showMenu)}/>
 
             {showMenu && 
-                <ThreeDotMenuDropdown>
-                    {children}
-                </ThreeDotMenuDropdown>
+                <>
+                    <div className='all-screen' onClick={() => setShowMenu(false)}></div>
+                    <div className="absolute right-4">
+                        <MenuDropdown>
+                            {children}
+                        </MenuDropdown>
+                    </div>
+                </>
             }
-        </>
+        </div>
     )
 }
