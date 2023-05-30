2561f38ada5ca7a9fa457cb7e69fafa0188acfa5
Erik Fubel <erik.fubel@gmail.com>
2023-05-30 20:23:22 +0200

    Add component for modal for creation of route

--- src/components/NewRouteModal.js
+++ src/components/NewRouteModal.js
@@ -2,9 +2,49 @@ import Modal from "./Modal";
 import FormInput from "./FormInput";
 import { useTafelApi } from "../contexts/ApiProvider";
 import { useToastContext } from "../contexts/ToastContext";
+import NewRouteNodesTable from "./NewRouteNodesTable";
+import { useEffect, useState } from "react";
+import RouteTypeSelector from "./RouteTypeSelector";
+import { useNavigate } from "react-router-dom";
 
 export default function NewRouteModal({ setShowModal }) {
+    const [availableNodes, setAvailableNodes] = useState()
+    const [selectedNodes, setSelectedNodes] = useState([])
+    const [routeType, setRouteType] = useState("")
+
+    // Fetch nodes data
     const api = useTafelApi();
+    useEffect(() => {
+        // UseEffect requires IIFE: https://developer.mozilla.org/en-US/docs/Glossary/IIFE
+        (async () => {
+            if (routeType === ""){
+                return
+            }
+            const response = await api.get('/' + routeType);
+            if (response.ok) {
+                // Filter out nodes that do not have an address
+                setAvailableNodes(response.body.filter(element => element.address !== null));
+            }
+            else {
+                setAvailableNodes(null);
+            }
+        })();
+    }, [api, routeType]);
+
+    const handleCheckboxChange = (event) => {
+      const checkboxId = event.target.id;
+  
+      if (event.target.checked) {
+        setSelectedNodes((prevCheckedIds) => [...prevCheckedIds, checkboxId]);
+      } else {
+        setSelectedNodes((prevCheckedIds) =>
+          prevCheckedIds.filter((id) => id !== checkboxId)
+        );
+      }
+    };
+
+    const navigate = useNavigate()
+
     const addToast = useToastContext();
     const handleCreate = (e) => {
         (async () => {
@@ -24,7 +64,9 @@ export default function NewRouteModal({ setShowModal }) {
         
             // Construct request body
             const postBody = {
-                "name": formJson['name']
+                "name": formJson['name'],
+                "type": routeType,
+                "nodes": selectedNodes
             }
 
             // Post to server
@@ -32,8 +74,8 @@ export default function NewRouteModal({ setShowModal }) {
 
             // Handle response
             if (response.ok) {
-                // TODO: redirect to route details page
-                addToast({text: "Created route", type: 'success'})
+                addToast({text: "Successfully created route", type: "success"})
+                navigate(`/routing/routes/${response.body}`)
             }
             else {
                 addToast({text: "Could not create route", type: 'error'})
@@ -43,10 +85,18 @@ export default function NewRouteModal({ setShowModal }) {
     return (
         <Modal setShowModal={setShowModal}>
             <h3 class="mb-4 text-xl font-medium text-gray-900 dark:text-white">Create new route</h3>
+            <RouteTypeSelector routeType={routeType} setRouteType={setRouteType}/>
+            {routeType !== "" &&
             <form class="space-y-6" onSubmit={handleCreate}>
                 <FormInput label={"Name"} type={"text"} required autofocus={true} />
-                <button type="submit" class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Create Route</button>
+                {(availableNodes !== undefined && availableNodes !== null)  &&
+                <>
+                    <NewRouteNodesTable nodes={availableNodes} nodeType={routeType} handleCheckboxChange={handleCheckboxChange} />
+                    <button type="submit" class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Create Route</button>
+                </>
+                }
             </form>
+            }
         </Modal>
     );
 };
