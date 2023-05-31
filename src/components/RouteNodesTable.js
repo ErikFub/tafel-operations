ac080ffa996b42b733ced6650549146102300df0
Erik Fubel <erik.fubel@gmail.com>
2023-05-31 20:36:45 +0200

    Improve column width distribution

--- src/components/RouteNodesTable.js
+++ src/components/RouteNodesTable.js
@@ -5,13 +5,13 @@ export default function RouteNodesTable({ nodes, nodeType }) {
             <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400 table-fixed">
                 <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                     <tr>
-                        <th scope="col" class="px-6 py-3">
+                        <th scope="col" class="px-6 py-3 md:w-28">
                             Position
                         </th>
                         <th scope="col" class="px-6 py-3">
                             Name
                         </th>
-                        <th scope="col" class="px-6 py-3">
+                        <th scope="col" class="px-6 py-3 w-1/2">
                             Address
                         </th>
                     </tr>
@@ -19,7 +19,7 @@ export default function RouteNodesTable({ nodes, nodeType }) {
                 <tbody>
                     {nodes.map((node, index) =>
                         <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-                        <td class="w-4 p-4">
+                        <td class="w-4 px-6 py-4 font-semibold">
                             {String.fromCharCode(index+65)}
                         </td>
                         <th scope="row" class="px-6 py-4 font-medium text-gray-900 dark:text-white">
