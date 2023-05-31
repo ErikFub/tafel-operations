ca2b89b2863598dc98fbe29a161baf897a629719
Erik Fubel <erik.fubel@gmail.com>
2023-05-31 20:00:09 +0200

    Remove non-functional column checkbox

--- src/components/NewRouteNodesTable.js
+++ src/components/NewRouteNodesTable.js
@@ -19,10 +19,6 @@ export default function NewRouteNodesTable({ nodes, nodeType, handleCheckboxChan
                 <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                     <tr>
                         <th scope="col" class="p-4 w-1/6">
-                            <div class="flex items-center">
-                                <input id="checkbox-all-search" type="checkbox" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
-                                <label for="checkbox-all-search" class="sr-only">checkbox</label>
-                            </div>
                         </th>
                         <th scope="col" class="px-6 py-3 w-2/6">
                             Name
