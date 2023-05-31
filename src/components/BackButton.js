c09f6b4a54e9ffddbae96c5442ef8a41156a88b0
Erik Fubel <erik.fubel@gmail.com>
2023-05-31 14:18:07 +0200

    Add option to alter text in back button

--- src/components/BackButton.js
+++ src/components/BackButton.js
@@ -2,13 +2,14 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { icon } from '@fortawesome/fontawesome-svg-core/import.macro'
 import { useNavigate } from "react-router-dom";
 
-export default function BackButton() {
+export default function BackButton({ text }) {
+    const textAdj = text === undefined ? "Back" : text
     let navigate = useNavigate();
     return (
         <>
           <button onClick={() => navigate(-1)} className="bg-none text-gray-500 font-semibold h-6 mb-2">
             <FontAwesomeIcon icon={icon({name: 'angle-left'})} className='w-5 mr-1'/>
-            Back
+            {textAdj}
           </button> 
         </>
     );
