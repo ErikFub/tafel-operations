ce0f44efdca8c8245f6d2765d8396355c8ac0180
Erik Fubel <erik.fubel@gmail.com>
2023-05-31 14:02:34 +0200

    Add route to map component

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,8 +1,52 @@
-import { GoogleMap, useLoadScript } from "@react-google-maps/api";
+/* global google */
+import { DirectionsRenderer, GoogleMap, useLoadScript } from "@react-google-maps/api";
 import { Spinner } from "flowbite-react";
-import { useMemo } from "react";
+import { useEffect, useMemo, useState } from "react";
 
-export default function Map({ lat, lon }) {
+
+function MapDirectionsRenderer({ places, travelMode }) {
+    const [directions, setDirections] = useState(null);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+    const waypoints = places.map(p => ({
+        location: { lat: p.lat, lng: p.lon },
+        stopover: true
+    }));
+    const origin = waypoints.shift().location;
+    const destination = waypoints.pop().location;
+
+    const directionsService = new google.maps.DirectionsService();
+    directionsService.route(
+        {
+            origin: origin,
+            destination: destination,
+            travelMode: travelMode,
+            waypoints: waypoints
+        },
+        (result, status) => {
+            console.log(result)
+            if (status === google.maps.DirectionsStatus.OK) {
+                setDirections(result);
+            } else {
+                setError(result);
+            }
+        }
+    );
+    }, [places, travelMode]);
+
+    if (error) {
+        return <h1>{error}</h1>;
+    }
+    return (
+        directions && (
+            <DirectionsRenderer directions={directions} />
+        )
+    );
+}
+
+
+export default function Map({ lat, lon, places }) {
     const center = useMemo(() => ({ lat: lat, lng: lon}), [lat, lon])
     const { isLoaded } = useLoadScript({googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY})
 
@@ -10,8 +54,18 @@ export default function Map({ lat, lon }) {
         <>
             {isLoaded ?
             <>
-                <GoogleMap zoom={10} center={center} mapContainerClassName="w-full h-64"></GoogleMap>
-                <p>lakdfaksdnf</p>
+                <GoogleMap 
+                    zoom={5} 
+                    center={center} 
+                    mapContainerClassName="w-full h-full"
+                    options={{
+                        streetViewControl: false, 
+                        disableDefaultUI: true,
+                        zoomControl: true,
+                    }}
+                >
+                    <MapDirectionsRenderer places={places} travelMode={google.maps.TravelMode.DRIVING} />
+                </GoogleMap>
             </>
             :
                 <Spinner />
