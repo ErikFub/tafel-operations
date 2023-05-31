f80422a295529b87aad46af78e0842da37a8617f
Erik Fubel <erik.fubel@gmail.com>
2023-05-31 19:49:45 +0200

    Make search bar of route node selection functional

--- src/components/NewRouteNodesTable.js
+++ src/components/NewRouteNodesTable.js
@@ -1,15 +1,19 @@
+import { useState } from "react"
+import { SearchBar } from "./SearchBar"
+
 export default function NewRouteNodesTable({ nodes, nodeType, handleCheckboxChange }) {
+    const [searchText, setSearchText] = useState("");
+
+    const filteredNodes = nodes.filter(node => 
+        (node.first_name + (nodeType === 'customers' ? " " : node.name) + node.last_name).toLowerCase().indexOf(searchText.toLowerCase()) !== -1 ||
+        (`${node.address.street}, ${node.address.zip} ${node.address.city}, ${node.address.country}`).toLowerCase().indexOf(searchText.toLowerCase()) !== -1
+    );
+
     // Adapted from https://flowbite.com/docs/components/tables/#table-search
     return (
         <div class="relative overflow-x-hidden shadow-md sm:rounded-lg">
             <div class="pb-0 bg-white dark:bg-gray-900">
-                <label for="table-search" class="sr-only">Search</label>
-                <div class="relative mt-1">
-                    <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-                        <svg class="w-5 h-5 text-gray-500 dark:text-gray-400" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd"></path></svg>
-                    </div>
-                    <input type="text" id="table-search" class="block p-2 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg w-full bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search for items" />
-                </div>
+                <SearchBar searchText={searchText} onSearchTextChange={setSearchText} />
             </div>
             <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400 table-fixed">
                 <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
@@ -29,7 +33,7 @@ export default function NewRouteNodesTable({ nodes, nodeType, handleCheckboxChan
                     </tr>
                 </thead>
                 <tbody>
-                    {nodes.map(node =>
+                    {filteredNodes.map(node =>
                         <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
                         <td class="w-4 p-4">
                             <div class="flex items-center">
