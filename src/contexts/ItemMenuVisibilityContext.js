84e0408d09be7f253d1aa6884e498e3f0cdb7a61
Erik Fubel <erik.fubel@gmail.com>
2023-06-02 11:43:22 +0200

    Remove misleading `ItemMenuVisibilityContextProvider`

--- src/contexts/ItemMenuVisibilityContext.js
+++ src/contexts/ItemMenuVisibilityContext.js
@@ -7,14 +7,6 @@ const ItemMenuVisibilityContext = createContext({
 
 export default ItemMenuVisibilityContext;
 
-export function ItemMenuVisibilityContextProvider({ children }) {
-    return (
-        <ItemMenuVisibilityContext.Provider value={null}>
-            {children}
-        </ItemMenuVisibilityContext.Provider>
-    );
-}
-
 export function useItemMenuVisibilityContext() {
     return useContext(ItemMenuVisibilityContext);
 }
